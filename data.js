a6782fa68c1f3779a16d580ce65cfd4e4925dd6e
suppsialk-cloud <suppsialk@gmail.com>
2025-12-17 11:09:41 +0330

    Sialk ceram

---
+++ data.js
@@ -0,0 +1,480 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0.01679786939416772,
+        "pitch": 0.05082111936258826,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0059313260243368,
+          "pitch": 0.23522428077491675,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 1.7695630759404066,
+          "pitch": 0.15261906054079333,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": 0.5530585834122412,
+          "pitch": 0.29358937096303706,
+          "rotation": 0,
+          "target": "5-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.36392731927158195,
+          "pitch": 0.5903853016354059,
+          "title": "Velora",
+          "text": "60*120<div><div>Nano Polished</div><div>Random Faces</div></div>"
+        },
+        {
+          "yaw": -2.921764581881064,
+          "pitch": -0.044157024385032884,
+          "title": "Nalina",
+          "text": "60*120<br><div>Nano Polished</div><div>Random Faces</div>"
+        },
+        {
+          "yaw": 0.07283792939597689,
+          "pitch": 0.1731092759854107,
+          "title": "Shailin",
+          "text": "60*120<br><div>Nano Polished</div><div>Random Faces</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.877938655748091,
+          "pitch": 0.24384912395046499,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": -2.0755049547647744,
+          "pitch": 0.2642965067001608,
+          "rotation": 0,
+          "target": "5-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.20884265343831032,
+          "pitch": 0.33370241016137925,
+          "title": "Nalina",
+          "text": "60*120<br><div>Nano Polished</div><div>Random Faces</div>"
+        },
+        {
+          "yaw": -1.4435541009129889,
+          "pitch": 0.8289695560501293,
+          "title": "Velora",
+          "text": "60*120<br><div>Nano Polished</div><div>Random Faces</div>"
+        },
+        {
+          "yaw": -2.8155455350005987,
+          "pitch": 0.6362833549887501,
+          "title": "Nalina",
+          "text": "60*120<div>Nano Pulish</div><div>Random Face</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0688057391004815,
+          "pitch": 0.23207746405113383,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": 0.9647123730549492,
+          "pitch": 0.46405247573750863,
+          "rotation": 0,
+          "target": "6-7"
+        },
+        {
+          "yaw": 0.1822627346839596,
+          "pitch": 0.34970471611148213,
+          "rotation": 0,
+          "target": "4-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3305400129351845,
+          "pitch": 0.6134967359874128,
+          "title": "Shailin",
+          "text": "60*120<br><div>Nano Polished</div><div>Random Faces</div>"
+        },
+        {
+          "yaw": -2.3538751535540925,
+          "pitch": 0.6204242993289331,
+          "title": "Velora",
+          "text": "80*80<br><div>Nano Polished</div><div>Random Faces</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0713508391324122,
+          "pitch": 0.4928199466374039,
+          "rotation": 0,
+          "target": "5-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5663724564027532,
+          "pitch": -0.056141274006876074,
+          "title": "Sertina Light",
+          "text": "60*120<br><div>Nano Polished</div><div>Random Faces</div>"
+        },
+        {
+          "yaw": -1.026865408624877,
+          "pitch": -0.23735203962192664,
+          "title": "Sertina Dark",
+          "text": "60*120<br><div>Nano Polished</div><div>Random Faces</div>"
+        },
+        {
+          "yaw": 1.5243246583736534,
+          "pitch": 0.04511463278166339,
+          "title": "Puzzle&nbsp;Ceramic 01",
+          "text": "Area : 17 CM"
+        },
+        {
+          "yaw": 0.06639809268434504,
+          "pitch": 0.4757930099869725,
+          "title": "Washbasin 01",
+          "text": "100*50*16"
+        },
+        {
+          "yaw": -0.8992048052196786,
+          "pitch": 0.8551222305166242,
+          "title": "Velora",
+          "text": "80*80<br><div>Nano Polished</div><div>Random Faces</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9776830085167667,
+          "pitch": 0.6033897031448099,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1833657430898104,
+          "pitch": 0.6971546400444559,
+          "title": "Washbasin 02",
+          "text": "100*50*17.5"
+        },
+        {
+          "yaw": -0.11832768643243696,
+          "pitch": -0.05006565622528569,
+          "title": "Elmora",
+          "text": "60*120<br><div>Nano Polished</div><div>Random Faces</div>"
+        },
+        {
+          "yaw": -2.3907977055251877,
+          "pitch": 1.0244684437387122,
+          "title": "Velora",
+          "text": "80*80<br><div>Nano Polished</div><div>Random Faces</div>"
+        }
+      ]
+    },
+    {
+      "id": "5-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6424692232126876,
+          "pitch": 0.36078970418017775,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": 0.8477707654334008,
+          "pitch": 0.2834027039813538,
+          "rotation": 0,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4391471268204654,
+          "pitch": -0.03992196340434617,
+          "title": "Colorado",
+          "text": "60*120<br><div>Nano Polished</div><div>Random Faces</div>"
+        },
+        {
+          "yaw": 0.9588234012289156,
+          "pitch": 0.67468482031917,
+          "title": "Velora",
+          "text": "80*80<br><div>Nano Polished</div><div>Random Faces</div>"
+        },
+        {
+          "yaw": -0.44361488311759345,
+          "pitch": -0.034082664813466224,
+          "title": "Puzzle&nbsp;Ceramic 07",
+          "text": "Area : 15.5 cm"
+        }
+      ]
+    },
+    {
+      "id": "6-7",
+      "name": "7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9883478548700095,
+          "pitch": 0.2961668948847276,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0939954609863562,
+          "pitch": 0.5132088403734318,
+          "title": "Ribera",
+          "text": "80*80<br><div>Nano Polished</div><div>Random Faces</div>"
+        },
+        {
+          "yaw": 2.7301930134493837,
+          "pitch": 0.010565553863347787,
+          "title": "Rivaz",
+          "text": "60*120<div>Matte</div><div>Random Faces</div>"
+        },
+        {
+          "yaw": 1.4424034359838398,
+          "pitch": 0.022498844593009437,
+          "title": "Black Rose",
+          "text": "60*120<br><div>Nano Polished</div><div>Random Faces</div>"
+        }
+      ]
+    }
+  ],
+  "name": "sialk ceram",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
